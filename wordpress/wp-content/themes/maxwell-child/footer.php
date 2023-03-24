7a9956c7a4ba2131dd082849d6edc80442c24065
alkeshrohit22 <alkesh.rohit@sigmainfo.net>
2023-03-24 15:37:25 +0530

    added todays works

---
+++ wordpress/wp-content/themes/maxwell-child/footer.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains all content after the main content area and sidebar
+ *
+ * @package Maxwell
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<?php do_action( 'maxwell_before_footer' ); ?>
+
+	<div id="footer" class="footer-wrap">
+
+		<footer id="colophon" class="site-footer container clearfix" role="contentinfo">
+            <div class="custom-footer">
+                <div class="footer-link">
+                    <h5>THE TIMES OF INDIA</h5>
+                        <!--Links -->
+                    <ul class="list1">
+                            <li><a href="https://www.timesinternet.in/">About us</a></li>
+                            <li><a href="https://ads.colombiaonline.com/expresso/selfservice/loginSelfService.htm">Create Your own <br> World</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/privacy-policy/cookiepolicy/86934312.cms">Privacy Policy</a></li>
+                            <li><a href="https://timesinternet.in/advertise/marketing">Advertise with us</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/archive.cms">Archives</a></li>
+                    </ul>
+                    <ul class="list2">
+                            <li><a href="https://timesofindia.indiatimes.com/rss.cms">RSS</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/newsletterhome.cms">NewsLetters</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/feedback.cms">Feedback</a></li>
+                            <li><a href="https://epaper.timesgroup.com/TOI/TimesOfIndia/index.html?a=c">ePaper</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sitemap.cms">SiteMap</a></li>
+                    </ul>
+
+                    <div class="footer-address">
+                        <h4>Address : </h4>
+                        <span>Location : India</span><br>
+                        <span>Headquarters : Mumbai, Maharashtra, India</span>
+                        <span>Mobile No : 079 6777 3300</span>
+                    </div>
+                </div>
+                <div class="footer-link2">
+                    <h5>Popular Categories</h5>
+                    <ul class="link1">
+                            <li><a href="https://timesofindia.indiatimes.com/home/headlines">Headlines</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sports">Sport News</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/business/india-business">Business News</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/india">India News</a></li>
+                            <li><a href="https://photogallery.indiatimes.com/celebs?_gl=1*1xdpyj8*_ga*MTMxNTI1Mzk2LjE2Nzk0ODAwMjE.*_ga_FCN624MN68*MTY3OTYzNzM1Ni45LjEuMTY3OTYzNzM2MC41Ni4wLjA.#_ga=2.41389222.562431997.1679480021-131525396.1679480021">Celebrity Photos</a></li>
+                    </ul>
+                    <ul class="link2">
+                            <li><a href="https://timesofindia.indiatimes.com/entertainment/hindi/bollywood/news">Bollywood News</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/life-style/health-fitness">Health & Fitness Tips</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/tv/hindi">Indian TV Shows</a></li>
+                                                </ul>
+                    <h5 class="second-h5">Top Trends</h5>
+                        <ul class="link3">
+                            <li><a href="https://timesofindia.indiatimes.com/india/coronavirus-in-india-covid-cases-today-live-updates-march-23-2023/liveblog/98927139.cms">Corona Virus <br>news</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/india/rahul-gandhi-defamation-case-modi-surname-remarks-surat-court-live-updates/liveblog/98930471.cms">Rahul Gandhi</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sports/badminton/pv-sindhu-crashes-out-of-swiss-open/articleshow/98958226.cms">PV Sindhu</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sports/cricket/wpl/wpl-eliminator-mumbai-indians-batting-vs-up-warriorzs-spin/articleshow/98956807.cms">WPL 2023</a></li>
+                        </ul>
+                        <ul class="link4">
+                            <li><a href="https://timesofindia.indiatimes.com/sports/cricket/ipl/top-stories/ipl-new-rule-allows-captains-naming-of-playing-xi-after-toss/articleshow/98915406.cms">IPL NEW Rules</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sports/cricket/ipl/schedule">IPL Schedule</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/sports/cricket/wpl/points-table">WPL Point Tabels</a></li>
+                            <li><a href="https://timesofindia.indiatimes.com/india/rahul-gandhis-disqualification-immediate-automatic-despite-bail-legal-experts/articleshow/98951616.cms">Rahul Gandhi Convicted</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-link3">
+                        <h5>HOT ON THE WEB</h5>
+                            <ul class="link1">
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/hindi/bollywood/news/rrr-star-jr-ntr-leaves-fans-in-shock-after-he-says-he-will-stop-doing-movies-if-/articleshow/98796150.cms">Jr. NTR</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/life-style/parenting/web-stories/baby-names-likely-to-go-extinct-in-2023/photostory/98539523.cms">Baby Name</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/telugu/web-stories/ten-captivating-pictures-of-ntr30-actress-janhvi-kapoor/photostory/98940310.cms">Janhvi Kapoor</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/tv/web-stories/hindi/from-swimsuits-to-bold-outfits-shweta-tiwari-raises-the-temperature-in-these-sultry-attires/photostory/98932670.cms">Shweta Tiwari</a></li>
+                            </ul>
+                            <ul class="link2">
+                                <li><a href="https://timesofindia.indiatimes.com/life-style/fashion/web-stories/miss-world-2022-karolina-bielawska-dons-a-lehenga-for-the-first-time-at-femina-miss-india-2023-awards-night/photostory/98921236.cms">Miss World 2022</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/life-style/fashion/web-stories/miss-world-2022-karolina-bielawska-dons-a-lehenga-for-the-first-time-at-femina-miss-india-2023-awards-night/photostory/98921236.cms">Deepthi Sunaina</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/telugu/web-stories/fans-go-gaga-over-mrunal-thakurs-looks-and-attires/photostory/98946196.cms">Mrunal Thakkar</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/life-style/fashion/web-stories/hottest-outfits-worn-by-raashii-khanna/photostory/98900859.cms">Raashi Khanna</a></li>
+                            </ul>
+                        <h5 class="footer-link3-h5-second">TRENDING TOPICS</h5>
+                            <ul class="link3">
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/punjabi/web-stories/nora-fatehis-top-fashion-forward-looks/photostory/98950941.cms">Nora Fatehi</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/hindi/bollywood/news/adnan-sami-opens-up-about-acquiring-indian-citizenship-people-in-pakistan-said-i-chose-india-because-i-was-getting-more-money/articleshow/98951820.cms">Adnan Sami</a></li>
+                                <li><a href="https://recipes.timesofindia.com/web-stories/navratri-2023-farali-aloo-paratha-recipe/photostory/98909767.cms?_gl=1*nc3wng*_ga*MTMxNTI1Mzk2LjE2Nzk0ODAwMjE.*_ga_FCN624MN68*MTY3OTYzNzM1Ni45LjEuMTY3OTYzNzM2MC41Ni4wLjA.">Aloo Paratha Recipe</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/hindi/web-stories/from-salman-khan-to-rekha-bollywood-celebs-who-remained-unmarried/photostory/98926039.cms">Salman Khan</a></li>
+                            </ul>
+                            <ul class="link4">
+                                <li><a href="https://timesofindia.indiatimes.com/life-style/relationships/web-stories/9-bad-habits-that-can-make-you-look-immature-at-work/photostory/98940258.cms">Bad Habit</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/entertainment/bhojpuri/web-stories/best-pics-of-bhojpuri-celebs-this-week/photostory/98752515.cms">Bhojpuri Celebs</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/gadgets-news/vivo-y55s-budget-smartphone-tipped-to-launch-on-this-date-images-and-specs-leaked/articleshow/87981530.cms">Vivo Y55s</a></li>
+                                <li><a href="https://timesofindia.indiatimes.com/most-searched-products/electronics/miscellaneous/wifi-routers-with-a-sim-card-slot-best-picks-for-seamless-internet-connection/articleshow/86811705.cms">Wifi Routers</a></li>
+                            </ul>
+                    </div>
+            </div>
+		</footer><!-- #colophon -->
+
+	</div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
